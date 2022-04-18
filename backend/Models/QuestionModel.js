a496ed2e49f7c9815c2400f27f6b91bd3164b293
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2022-04-18 14:01:29 -0700

    Question Model

---
+++ backend/Models/QuestionModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+//schema
+const questionSchema = new Schema({
+        title: {type: String, required: true},
+        body: {type: String, required: true},
+        tags: {type: Array},
+        status: {type: String, default: "unanswered"},
+    },
+
+    {
+        versionKey: false
+    });
+
+
+//model based on schema
+const QuestionModel = mongoose.model('question', questionSchema);
+module.exports = QuestionModel;
