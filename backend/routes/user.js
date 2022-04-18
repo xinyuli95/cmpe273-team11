ebe1885b4913cb75bcfb8c097ccd87b7bc9f8cf5
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2022-04-18 15:19:51 -0700

    added mysql

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,124 +3,85 @@ const express = require("express");
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const {secret} = require('../Utils/config');
-const User = require('../Models/UserModel');
+// const User = require('../Models/UserModel');
 const {auth} = require("../utils/passport");
 auth();
 
-//signup
-router.post('/signup', async (req, res) => {
-    console.log("Inside SiGN UP POST");
+//mysql
 
-    const newUser = new User({
-        username: req.body.username,
-        password: req.body.password,
-
-    });
-
-    console.log(req.body.username);
-    console.log(req.body.password);
-
-    User.findOne({username: req.body.username}, (err, userTaken) => {
-        if (err) {
-            console.log(err);
-            console.log("ERROR SIGNING UP");
-            res.writeHead(500, {
-                'Content-Type': 'text/plain'
-            })
-            res.end();
-        }
-        if (userTaken) {
-            res.writeHead(400, {
-                'Content-Type': 'text/plain'
-            })
-            res.end("Username already exists");
-        } else {
-            newUser.save((error, data) => {
-                if (error) {
-                    res.writeHead(500, {
-                        'Content-Type': 'text/plain'
-                    })
-                    res.end();
-                } else {
-                    console.log("sign up works");
-                    res.writeHead(200, {
-                        'Content-Type': 'text/plain'
-                    })
-                    res.end(data);
-                }
-            });
-        }
-    });
-
-
-});
-
-
-//login
 router.post('/login', async (req, res) => {
-
-    console.log("INSIDE LOGIN");
     const usernameValue = req.body.username;
     const passwordValue = req.body.password;
     console.log(usernameValue);
     console.log(passwordValue);
 
-    User.findOne({username: req.body.username, password: req.body.password}, (error, user) => {
-        if (error) {
-            console.log("LOGIN NOT WORKING");
-            res.writeHead(500, {
-                'Content-Type': 'text/plain'
-            })
-            res.end("Error Occurred");
-        }
-        if (user) {
-            console.log("LOGIN WORKING");
-            // res.cookie('cookie', user.username, { maxAge: 900000, httpOnly: false, path: '/' });
-            // req.session.user = user;
+    await db.query(
+        "SELECT * FROM users WHERE username = ? AND password = ?", [usernameValue, passwordValue],
+        function (err, result) {
+            if (err) {
+                console.log(err);
+                console.log("LOGIN NOT WORKING")
+            }
+
+            if (result.length > 0) {
+                //jwt
+                const payload = {_id: user._id, username: user.username};
+                const token = jwt.sign(payload, secret, {
+                    expiresIn: 1008000
+                });
+                // res.cookie('cookie', "admin", {maxAge: 900000, httpOnly: false, path: '/'});
+                // req.session.user = result;
+                // req.session.username = req.body.username;
+                // console.log(req.session.user);
 
 
-            //jwt
-            const payload = {_id: user._id, username: user.username};
-            const token = jwt.sign(payload, secret, {
-                expiresIn: 1008000
-            });
-            res.writeHead(200, {
-                'Content-Type': 'text/plain'
-            })
-            res.status(200).end("JWT " + token);
-        } else {
-            res.writeHead(401, {
-                'Content-Type': 'text/plain'
-            })
-            res.end("Invalid Credentials");
-            console.log("login failed");
-        }
-    });
+                res.status(200).end("JWT " + token);
+                console.log("LOGIN WORKING")
+                req.session.username = req.body.username;
+                console.log(req.session.username)
+            } else { //no username or password found
+                res.writeHead(400, {
+                    'Content-Type': 'text/plain'
+                })
+                res.end("Invalid credentials");
+                console.log("login failed")
+            }
+        });
 
 
 });
 
+//signup
+router.post('/signup', async (req, res) => {
+    console.log("Inside SiGN UP POST");
 
-//update profile
-router.put("/update/:id", async (req, res) => {
+    // console.log(Users);
+    // console.log(Users.map(x => x.username));
+    // console.log(req.body.password);
+    const name = req.body.name;
+    const username = req.body.username
+    const password = req.body.password;
+    console.log(req.body.name);
+    console.log(req.body.username);
+    console.log(req.body.password);
 
-    try {
-        const updateUser = await User.findByIdAndUpdate(
-            req.params.id,
-            {
-                $set: req.body,
-            },
-            {new: true}
-        );
-        console.log("profile update works");
-        res.status(200).json(updateUser);
-        res.end("Successful PROFILE UPDATE");
-    } catch (err) {
-        console.log(err);
-        console.log("ERROR UPDATING PROFILE");
-        res.status(500).json(err);
 
-    }
+    await db.query("INSERT INTO users (name, username, password) VALUES (?,?, ?)", [name, username, password], function (err, result) {
+        if (err) {
+            console.log(err);
+            console.log("ERROR SIGNING UP");
+            res.writeHead(400, {
+                'Content-Type': 'text/plain'
+            })
+            res.end("Error while signing up");
+        } else {
+            console.log("sign up works");
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            })
+            res.end("Successful Sign up");
+        }
+    });
 
 
 });
