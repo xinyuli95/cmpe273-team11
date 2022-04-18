e1e78afa96dfbc10faa24f2af21d5e02446f0cb1
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2022-04-18 13:47:21 -0700

    mongodb

--- backend/Utils/config.js
+++ backend/Utils/config.js
@@ -1,15 +1,8 @@
-const mysql = require('mysql');
+const config = {
+    secret: "cmpe273_secret_key",
+    frontendURL: "http://localhost:3000",
+    mongoDB: 'mongodb+srv://admin:<password>@<cluster_id>.mongodb.net/<collection>?retryWrites=true&w=majority'
+};
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    // port: '',
-    database: 'StackOverflow',
-});
 
-db.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected");
-});
-module.exports = db;
+module.exports = config;
