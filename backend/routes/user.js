8bb91290a77ab8a4a1152d2088ff7658d0fff8b8
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2022-05-02 19:37:59 -0700

    user routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,8 +3,8 @@ const express = require("express");
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const {secret} = require('../Utils/config');
-// const User = require('../Models/UserModel');
-const {db} = require('../Utils/mysqlConfig');
+const User = require('../Models/UserModel');
+const db = require('../Utils/mysqlConfig');
 const {auth} = require("../utils/passport");
 auth();
 
@@ -26,7 +26,7 @@ router.post('/login', async (req, res) => {
 
             if (result.length > 0) {
                 //jwt
-                const payload = {_id: user._id, username: user.username};
+                const payload = {_id: users._id, username: users.username};
                 const token = jwt.sign(payload, secret, {
                     expiresIn: 1008000
                 });
@@ -62,12 +62,12 @@ router.post('/signup', async (req, res) => {
     // const name = req.body.name;
     const username = req.body.username
     const password = req.body.password;
-    console.log(req.body.name);
+    // console.log(req.body.name);
     console.log(req.body.username);
     console.log(req.body.password);
 
 
-    await db.query("INSERT INTO users (username, password) VALUES (?, ?)", [name, username, password], function (err, result) {
+    await db.query("INSERT INTO users (username, password) VALUES (?, ?)", [username, password], function (err, result) {
         if (err) {
             console.log(err);
             console.log("ERROR SIGNING UP");
@@ -88,36 +88,76 @@ router.post('/signup', async (req, res) => {
 });
 
 
+//update profile
+router.put("/update/:id", async (req, res) => {
+    try {
+        const updateUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            {new: true}
+        );
+        console.log("profile update works");
+        res.status(200).json(updateUser);
+        res.end("Successful PROFILE UPDATE");
+    } catch (err) {
+        console.log(err);
+        console.log("ERROR UPDATING PROFILE");
+        res.status(500).json(err);
 
-//get users
-router.get('/userList/:id', async (req, res) => {
-    console.log("Inside GET USER request");
-
-
-    await db.query(
-        "SELECT * FROM users",
-        function (err, result) {
-            if (err) {
-                console.log(err);
-                console.log("GET user NOT WORKING")
-            }
-
-            if (result.length > 0) {
-                res.send(result);
-                console.log("GET users WORKING")
-
-            } else { //no username or password found
-                res.writeHead(400, {
-                    'Content-Type': 'text/plain'
-                })
-                console.log("GET req not working");
-            }
-        });
-
+    }
 
 
 });
 
+//Get user
+router.get("/find/:id", async (req, res) => {
+    console.log("INSIDE USER GET");
+
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user);
+        console.log("SUCCESS USER GET")
+
+    } catch (err) {
+        console.log("ERROR USER GET")
+        res.status(500).json(err);
+    }
+});
+
+//GET all users or specific user
+router.get("/userList", async (req, res) => {
+    console.log("INSIDE USER GET AlL");
+
+    const query = req.query.new;
+    const findUser = req.query.username;
+
+
+    try {
+        let users;
+
+        if (query) {
+            users = await User.find().sort({createdAt: -1}).limit(10);
+        }
+        else if (users) {
+            users = await User.find({
+                username: {
+                    $in: [findUser],
+                },
+            });
+        }
+        else {
+            users = await User.find();
+        }
+
+        console.log("SUCCESS GET USERS REQ");
+        res.status(200).json(users);
+    } catch (err) {
+        console.log("ERROR GET USERS REQ");
+        res.status(500).json(err);
+    }
+});
 
 
 module.exports = router;
