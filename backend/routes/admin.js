be7fa224226ce26216fdb7a1ae840f6b02e37a23
xinyuli95 <li_xinyu95@hotmail.com>
2022-05-05 21:38:03 -0700

    BACKEND Admin: add top users

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -16,33 +16,17 @@ router.get("/analytics", async (req, res) => {
         const questionCount = await Question.countDocuments({_id : { $gt : mongoose.Types.ObjectId(Math.floor(new Date(gmtDate.getFullYear()+'/'+(gmtDate.getMonth()+1)+'/'+gmtDate.getDate())/1000).toString(16)+"0000000000000000") }});
         const topQuestions = await Question.find().sort({views: -1}).limit(10);
         const topTags = await Tag.find().sort({totalCount: -1}).limit(10);
+        const topHighUsers = await User.find().sort({reputation: -1}).limit(10);
+        const topLowUsers = await User.find().sort({reputation: 1}).limit(10);
 
         res.status(200).json({  "questionCount": questionCount,
                                 "topQuestions": topQuestions,
-                                "topTags": topTags             
+                                "topTags": topTags,
+                                "topHighUsers": topHighUsers,
+                                "topLowUsers": topLowUsers      
                             });
     } catch (err) {
         console.log("ERROR GETTING analytics");
-        res.status(500).json(err);
-    }
-});
-
-//update answer
-router.put("/update/:id", async (req, res) => {
-    console.log("INSIDE answer UPDATE");
-
-    try {
-        const updatedAnswer = await Answer.findByIdAndUpdate(
-            req.params.id,
-            {
-                $set: req.body,
-            },
-            {new: true}
-        );
-        console.log("SUCCESS UPDATING answer")
-        res.status(200).json(updatedAnswer);
-    } catch (err) {
-        console.log("ERROR UPDATING answer");
         res.status(500).json(err);
     }
 });
