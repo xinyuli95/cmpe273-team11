022342138c4c50abafcba9258e587821ee72227c
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2022-05-05 18:19:39 -0700

    user

--- backend/Models/UserModel.js
+++ backend/Models/UserModel.js
@@ -5,10 +5,18 @@ const Schema = mongoose.Schema;
 //schema
 const usersSchema = new Schema({
         username: {type: String, required: true},
-        password: {type: String, required: true}
+        password: {type: String, required: true},
+        name: {type: String},
+        img: {type: String},
+        lastSeen: {type: Date},
+        about: {type: String},
+        badges: {type: Array},
+        tags: {type: Array},
+        bookmarks: {type: Array},
+
     },
     {
-        versionKey: false
+        timestamps: true
     });
 
 
