b64004deed73ce3858e8d744e0d7a8cbadff8d3a
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-05-09 22:13:33 -0700

    index.js  changes, removed unneccessary code

--- frontend/src/components/Auth/index.js
+++ frontend/src/components/Auth/index.js
@@ -1,11 +1,5 @@
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-} from "firebase/auth";
 import React, { useState } from "react";
-import {useHistory } from "react-router-dom";
-import { auth, provider } from "../../firebase";
+import { useHistory } from "react-router-dom";
 import "./index.css";
 
 function Index() {
@@ -24,101 +18,9 @@ function Index() {
     } else return true;
   }
 
-  const handleGoogleSignIN = () => {
-    setLoading(true);
-    signInWithPopup(auth, provider)
-      .then((res) => {
-        setLoading(false);
-        // console.log(res);
-        history.push("/");
-        // return (
-        //   <>
-
-        //   </>
-        // );
-      })
-      .catch((error) => {
-        setLoading(false);
-        console.log(error);
-      });
-  };
-
-  const handleSignIn = () => {
-    setError();
-    setLoading(true);
-    if (email === "" || password === "") {
-      setError("Required field is missing");
-      setLoading(false);
-    } else if (!validateEmail(email)) {
-      setError("Email is malformed");
-      setLoading(false);
-    } else {
-      signInWithEmailAndPassword(auth, email, password)
-        .then((res) => {
-          // console.log(res);
-          history.push("/");
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.log(error.code);
-          setError(error.message);
-          setLoading(false);
-        });
-    }
-  };
-
-  const handleRegister = () => {
-    setError("");
-    setLoading(false);
-    if (email === "" || password === "" || username === "") {
-      setError("Required field is missing.");
-      setLoading(false);
-    } else if (!validateEmail(email)) {
-      setError("Email is malformed");
-      setLoading(false);
-    } else {
-      createUserWithEmailAndPassword(auth, email, password)
-        .then((res) => {
-          // console.log(res);
-          history.push("/");
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.log(error);
-          setError(error.message);
-          setLoading(false);
-        });
-    }
-  };
   return (
     <div className="auth">
       <div className="auth-container">
-
-        {/* <div className="sign-options">
-          <div onClick={handleGoogleSignIN} className="single-option">
-            <img
-              alt="google"
-              src="https://image.flaticon.com/icons/png/512/281/281764.png"
-            />
-            <p>{loading ? "Signing in..." : "Login with Google"}</p>
-          </div>
-          <div className="single-option">
-            <img
-              alt="github"
-              src="https://image.flaticon.com/icons/png/512/270/270798.png"
-            />
-            <p>Login with Github</p>
-          </div>
-          <div className="single-option">
-            <img
-              alt="facebook"
-              src="https://image.flaticon.com/icons/png/512/733/733547.png"
-            />
-            <p>Login with Facebook</p>
-          </div>
-        </div> */}
-
-
         <div className="auth-login">
           <div className="auth-login-container">
             {register ? (
@@ -149,7 +51,7 @@ function Index() {
                   />
                 </div>
                 <button
-                  onClick={handleRegister}
+                  // onClick={handleRegister}
                   disabled={loading}
                   style={{
                     marginTop: "10px",
@@ -169,7 +71,7 @@ function Index() {
                   <input type="password" />
                 </div>
                 <button
-                  onClick={handleSignIn}
+                  // onClick={handleSignIn}
                   disabled={loading}
                   style={{
                     marginTop: "10px",
@@ -194,7 +96,7 @@ function Index() {
             </p>
           </div>
         </div>
-        
+
         {error !== "" && (
           <p
             style={{
