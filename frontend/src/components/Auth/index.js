1fb151db8f620637856dd46166ebdf23a43fadfa
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2022-05-11 15:15:17 -0700

    Fixed the login/signup component

--- frontend/src/components/Auth/index.js
+++ frontend/src/components/Auth/index.js
@@ -28,9 +28,9 @@ function Index() {
       setError("Required field is missing");
       setLoading(false);
 
-    } else if (!validateEmail(email)) {
-      setError("Email is malformed");
-      setLoading(false);
+      // } else if (!validateEmail(email)) {
+      //   setError("Email is malformed");
+      //   setLoading(false);
 
     } else {
       const bodyJSON = {
@@ -38,18 +38,19 @@ function Index() {
         password: password
       };
       await axios
-        .post("/api/user/login", bodyJSON)
-        .then((res) => {
-          console.log(res.data);
-          // alert("Successfully signed in");
-          history.push("/");
-          setLoading(false);
-        })
-        .catch((err) => {
-          console.log(err);
-          setError(error.message);
-          setLoading(false);
-        });
+          .post("/api/user/login", bodyJSON)
+          .then((res) => {
+            console.log(username);
+            console.log(res.data);
+            // alert("Successfully signed in");
+            history.push("/");
+            setLoading(false);
+          })
+          .catch((err) => {
+            console.log(err);
+            setError(error.message);
+            setLoading(false);
+          });
     }
   };
 
@@ -61,9 +62,9 @@ function Index() {
       setError("Required field is missing.");
       setLoading(false);
 
-    } else if (!validateEmail(email)) {
-      setError("Email is malformed");
-      setLoading(false);
+      // } else if (!validateEmail(email)) {
+      //   setError("Email is malformed");
+      //   setLoading(false);
 
     } else {
       const bodyJSON = {
@@ -72,120 +73,120 @@ function Index() {
         email: email
       };
       await axios
-        .post("/api/user/signup", bodyJSON)
-        .then((res) => {
-          console.log(res);
-          history.push("/");
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.log(error);
-          setError(error.message);
-          setLoading(false);
-        });
+          .post("/api/user/signup", bodyJSON)
+          .then((res) => {
+            console.log(res);
+            history.push("/");
+            setLoading(false);
+          })
+          .catch((error) => {
+            console.log(error);
+            setError(error.message);
+            setLoading(false);
+          });
     }
-    
+
   };
 
   return (
-    <div className="auth">
-      <div className="auth-container">
-        <div className="auth-login">
-          <div className="auth-login-container">
-            {register ? (
-              <>
-                {" "}
-                <div className="input-field">
-                  <p>Username</p>
-                  <input
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    type="text"
-                  />
-                </div>
-                <div className="input-field">
-                  <p>Email</p>
-                  <input
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    type="text"
-                  />
-                </div>
-                <div className="input-field">
-                  <p>Password</p>
-                  <input
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    type="password"
-                  />
-                </div>
-                <button
-                  onClick={handleRegister}
-                  disabled={loading}
-                  style={{
-                    marginTop: "10px",
-                  }}
-                >
-                  {loading ? "Registering..." : "Register"}
-                </button>
-              </>
-            ) : (
-              <>
-                <div className="input-field">
-                  <p>Email</p>
-                  <input 
-                     value={email}
-                     onChange={(e) => setEmail(e.target.value)}
-                     type="text"
-                  />
-                </div>
-                <div className="input-field">
-                  <p>Password</p>
-                  <input 
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    type="password"
-                  />
-                </div>
-                <button
-                  onClick={handleSignIn}
-                  disabled={loading}
-                  style={{
-                    marginTop: "10px",
-                  }}
-                >
-                  {loading ? "Logging in..." : "Login"}
-                </button>
-              </>
-            )}
+      <div className="auth">
+        <div className="auth-container">
+          <div className="auth-login">
+            <div className="auth-login-container">
+              {register ? (
+                  <>
+                    {" "}
+                    <div className="input-field">
+                      <p>Username</p>
+                      <input
+                          value={username}
+                          onChange={(e) => setUsername(e.target.value)}
+                          type="text"
+                      />
+                    </div>
+                    <div className="input-field">
+                      <p>Email</p>
+                      <input
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                          type="text"
+                      />
+                    </div>
+                    <div className="input-field">
+                      <p>Password</p>
+                      <input
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                          type="password"
+                      />
+                    </div>
+                    <button
+                        onClick={handleRegister}
+                        disabled={loading}
+                        style={{
+                          marginTop: "10px",
+                        }}
+                    >
+                      {loading ? "Registering..." : "Register"}
+                    </button>
+                  </>
+              ) : (
+                  <>
+                    <div className="input-field">
+                      <p>Email</p>
+                      <input
+                          value={username}
+                          onChange={(e) => setUsername(e.target.value)}
+                          type="text"
+                      />
+                    </div>
+                    <div className="input-field">
+                      <p>Password</p>
+                      <input
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                          type="password"
+                      />
+                    </div>
+                    <button
+                        onClick={handleSignIn}
+                        disabled={loading}
+                        style={{
+                          marginTop: "10px",
+                        }}
+                    >
+                      {loading ? "Logging in..." : "Login"}
+                    </button>
+                  </>
+              )}
 
-            <p
-              onClick={() => setRegister(!register)}
-              style={{
-                marginTop: "10px",
-                textAlign: "center",
-                color: "#0095ff",
-                textDecoration: "underline",
-                cursor: "pointer",
-              }}
-            >
-              {register ? "Login" : "Register"} ?
-            </p>
+              <p
+                  onClick={() => setRegister(!register)}
+                  style={{
+                    marginTop: "10px",
+                    textAlign: "center",
+                    color: "#0095ff",
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                  }}
+              >
+                {register ? "Login" : "Register"} ?
+              </p>
+            </div>
           </div>
-        </div>
 
-        {error !== "" && (
-          <p
-            style={{
-              color: "red",
-              fontSize: "14px",
-            }}
-          >
-            {error}
-          </p>
-        )}
+          {error !== "" && (
+              <p
+                  style={{
+                    color: "red",
+                    fontSize: "14px",
+                  }}
+              >
+                {error}
+              </p>
+          )}
+        </div>
       </div>
-    </div>
   );
 }
 
