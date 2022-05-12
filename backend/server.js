83fa88fe25e9cb90ae3c4d5c184d9c3c8de0e838
Xinyu Li <li_xinyu95@hotmail.com>
2022-05-12 09:49:16 -0700

    fix port mismatch

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const path = require("path");
 const app = express();
 const router = require("./routers");
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3001;
 
 const db = require("./db");
 db.connect();
