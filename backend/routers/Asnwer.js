b962d4401e773808ee31cb60c58ffdbca9674d0d
xinyuli95 <li_xinyu95@hotmail.com>
2022-05-13 12:13:53 -0700

    revert unnecessary changes: Asnwer.js

--- backend/routers/Asnwer.js
+++ backend/routers/Asnwer.js
@@ -2,23 +2,12 @@ const express = require("express");
 const router = express.Router();
 const answerDB = require("../models/Answers");
 
-/*
 router.post("/", async (req, res) => {
   const answerData = new answerDB({
     question_id: req.body.question_id,
     answer: req.body.answer,
     user: req.body.user,
   });
-*/
-
-router.get("/", async (req, res) => {
-  answerDB.find((err, data) => {
-
-      if (err) {
-          res.status(400).send({
-              message: err,
-          });
-      }
 
   await answerData
     .save()
